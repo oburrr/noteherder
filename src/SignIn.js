d89dd04c60562648dadf09d4527902e213963bae
Olivia Burris <oliviaburris11@gmail.com>
2017-07-11 09:37:47 -0400

    add github login

--- src/SignIn.js
+++ src/SignIn.js
@@ -3,11 +3,11 @@ import React from 'react'
 import './SignIn.css'
 import quill from './quill.svg'
 import googleLogo from './google.svg'
-import { auth, googleProvider } from './base'
+import { auth, googleProvider, githubProvider } from './base'
 
 const SignIn = () => {
-  const authenticate = () => {
-    auth.signInWithPopup(googleProvider)
+  const authenticate = (provider) => {
+    auth.signInWithPopup(provider)
   }
 
   return (
@@ -21,10 +21,17 @@ const SignIn = () => {
         <p>You never know when you'll need to write crap down. In fact, you should probably be taking notes right now.</p>
         <button
           className="google"
-          onClick={authenticate}
+          onClick={() => authenticate(googleProvider)}
         >
           <img src={googleLogo} alt="" />
           Sign in with Google
+        </button>
+        <button
+          className="github"
+          onClick={() => authenticate(githubProvider)}
+        >
+          <i className="fa fa-github"></i>
+          Sign in with GitHub
         </button>
       </main>
     </div>
